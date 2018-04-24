78427dd6a9ee2f174e7564d98a585ad5651e5581
JoJeonghoon <akima95@naver.com>
2018-04-24 10:50:26 +0900

    Add a new faculty JJH

--- src/main/java/edu/handong/csee/java/example/inheritance/PolymorphismDemonstrator.java
+++ src/main/java/edu/handong/csee/java/example/inheritance/PolymorphismDemonstrator.java
@@ -8,6 +8,7 @@ public class PolymorphismDemonstrator {
 		people[1] = new Undergraduate("Kick, Anita", 9931, 2);
 		people[2] = new Student("DeBanque, Robin", 8812);
 		people[3] = new Undergraduate("Bugg, June", 9901, 4);
+		people[4] = new Faculty("JeongHoon, Jo", 3601);
 
 		for (Person p : people) {
 			p.writeOutput();
